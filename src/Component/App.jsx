a7e18cc146e850e2c23ceae320497252b53be318
Angel-Banks <angelonuchuks@gmail.com>
2022-08-20 17:15:04 +0100

    updatedContact.js

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Card from "./Card";
 
+
+import Avatar from "./Avatar";
+
 import contacts from "../contacts";
+
+function createCard (contact){
+
+  return <Card
+    id = {contact.id}
+    key = {contact.id}
+    name = {contact.name}
+    img = {contact.imgURL}
+    tel = {contact.phone}
+    email = {contact.email}
+    
+  />
+}
 
 function App (){
 
     return (
         <div>
     <h1 className = "heading">My Contacts</h1>
-    <Card
+
+    <Avatar img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFidJ_--dSRcm9Dd_R0rR7Xcg3u4oKNJOV5mz8rYvNMw&s"/>
+
+      {contacts.map(createCard)}
+
+   
+    {/* <Card
       name={contacts[0].name}
       img={contacts[0].imgURL}
       tel = {contacts [0].phone}
@@ -27,7 +49,7 @@ function App (){
       img={contacts[2].imgURL}
       tel = {contacts [2].phone}
       email={contacts[2].email}
-    />
+    /> */}
   </div>
     )
 }
